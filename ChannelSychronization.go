147e6df34cdbe26fdc4b8f83474bf09016bd2e7e
LiRongji1995 <LiRongji1995@Gmail.com>
2024-09-10 08:43:53 +0800

    ChannelSychronization

--- ChannelSychronization.go
+++ ChannelSychronization.go
@@ -16,5 +16,11 @@ func main() {
 	done := make(chan bool, 1)
 	go worker(done)
 
+	/**
+	作用一，从 done 通道接收一个值：这表示主 goroutine 正在等待 worker goroutine
+	完成任务并发送信号到 done 通道。作用二，阻塞主 goroutine：
+	直到 worker goroutine 发送信号，主 goroutine 才会继续执行。
+	如果没有 <-done，主 goroutine 不会再等待 worker goroutine 完成。
+	*/
 	<-done
 }
