29c750c1eab41c6b8624c9d0f8ef9e869dae4318
NK007 <lirongji1995@gmail.com>
2024-09-17 11:50:37 +0800

    s -> s1

--- StringFunctions.go
+++ StringFunctions.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	s "strings"
+	s1 "strings"
 )
 
 var p = fmt.Println
 
 func main() {
 
-	p("Contains:  ", s.Contains("test", "es"))
-	p("Count:     ", s.Count("test", "t"))
-	p("HasPrefix: ", s.HasPrefix("test", "te"))
-	p("HasSuffix: ", s.HasSuffix("test", "st"))
-	p("Index:     ", s.Index("test", "e"))
-	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
-	p("Repeat:    ", s.Repeat("a", 5))
-	p("Replace:   ", s.Replace("foo", "o", "0", -1))
-	p("Replace:   ", s.Replace("foo", "o", "0", 1))
-	p("Split:     ", s.Split("a-b-c-d-e", "-"))
-	p("ToLower:   ", s.ToLower("TEST"))
-	p("ToUpper:   ", s.ToUpper("test"))
+	p("Contains:  ", s1.Contains("test", "es"))
+	p("Count:     ", s1.Count("test", "t"))
+	p("HasPrefix: ", s1.HasPrefix("test", "te"))
+	p("HasSuffix: ", s1.HasSuffix("test", "st"))
+	p("Index:     ", s1.Index("test", "e"))
+	p("Join:      ", s1.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s1.Repeat("a", 5))
+	p("Replace:   ", s1.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s1.Replace("foo", "o", "0", 1))
+	p("Split:     ", s1.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s1.ToLower("TEST"))
+	p("ToUpper:   ", s1.ToUpper("test"))
 }
